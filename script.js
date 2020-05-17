d885a4c07fbfed4d210cf266e356f348953ed109
Keith Fike <fkeithr@gmail.com>
2020-05-17 17:32:07 -0600

    added password length function

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 // Assignment code here
 
+var length = 0;
+
+//prompt for criteria
+
+//password length
+function passwordLength() {
+  length = window.prompt("Enter a password length between 8 and 128 characters")
+
+  length = length.valueOf();
+
+  if (length >= 8 && length <= 128) {
+    characterType();
+  } else {
+    window.alert("Please enter a valid number!");
+
+    passwordLength();
+  }
+}
+//character types
+function characterType() {
+  console.log(length);
+}
+//generate password
+
+passwordLength();
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
