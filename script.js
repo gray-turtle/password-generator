8b4c790f02957758f207c9bf3ce25299f0c7a3a0
Keith Fike <fkeithr@gmail.com>
2020-05-17 18:20:47 -0600

    added password generator function

--- script.js
+++ script.js
@@ -12,8 +12,6 @@ var special = false;
 function passwordLength() {
   length = window.prompt("Enter a password length between 8 and 128 characters")
 
-  length = length.valueOf();
-
   if (length >= 8 && length <= 128) {
     characterType();
   } else {
@@ -41,8 +39,51 @@ function characterType() {
     special = true;
   }
 
+  if (uppercase === true || lowercase === true || numeric === true || special === true) {
+    generatePassword();
+  } else {
+    window.alert("You must select at least one option!");
+
+    characterType();
+  }
+
 }
 //generate password
+
+function generatePassword() {
+  var lowercase_string = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase_string = lowercase_string.toUpperCase();
+  var numeric_string = "0123456789";
+  var special_string = "!#%')+-/;=?[]_{}";
+  var current_pass = "";
+
+  for (i = 0; i < length; i++) {
+
+    var rand = Math.floor(Math.random() * 4);
+
+    switch (rand) {
+      case 0:
+        current_pass += lowercase_string.charAt(Math.floor(Math.random() * lowercase_string.length));
+      
+        break;
+      case 1:
+        current_pass += uppercase_string.charAt(Math.floor(Math.random() * uppercase_string.length));
+      
+        break;
+      case 2:
+        current_pass += numeric_string.charAt(Math.floor(Math.random() * numeric_string.length));
+      
+        break;
+      case 3:
+        current_pass += special_string.charAt(Math.floor(Math.random() * special_string.length));
+
+        break;
+    }
+  }
+
+  console.log(current_pass);
+
+}
 
 passwordLength();
 
